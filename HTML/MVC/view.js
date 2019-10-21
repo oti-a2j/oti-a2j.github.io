9de3c35d6902c86aa3f7727536573e9da7ea649f
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-10-21 15:22:59 +0300

    primul mvc

---
+++ HTML/MVC/view.js
@@ -0,0 +1,38 @@
+class t_eq2_view
+{
+	events;
+	
+	constructor()
+	{
+		document.getElementById("id_button").addEventListener("click", this.on_solve_clicked);
+	}
+	
+	set_events(events)
+	{
+		this.events = events;
+	}
+	
+	on_solve_cliked()
+	{
+		//ar trebui sa apelam functia on_solve din consola, dar nu putem direct
+		//din lisat de evenimente de functii apelam functia on solve
+		this.events.call_event("on_solve_clicked_uab");
+	}
+	get_a()
+	{
+		return document.getElementById("id_a").value
+	}
+	get_b()
+	{
+		return document.getElementById("id_b").value
+	}
+	get_c()
+	{
+		return document.getElementById("id_c").value
+	}
+	set_solutions_para(solutions)
+	{
+		document.getElementById("id_x1").innerHTML = solutions.x1_re + "+" + solutions.x1_im + "i";
+		document.getElementById("id_x2").innerHTML = solutions.x2_re + "+" + solutions.x2_im + "i";
+	}
+}
