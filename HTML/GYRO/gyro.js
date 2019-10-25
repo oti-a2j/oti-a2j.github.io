af8b9a34a6cd1cb8b7c9afab0ec7ce4e5dad3c71
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-10-25 13:40:55 +0300

    acceleration adding ok

--- HTML/GYRO/gyro.js
+++ HTML/GYRO/gyro.js
@@ -1,7 +1,7 @@
 document.getElementById("id_logic_version").innerHTML = "Logic Version = 2019.10.25.0";
 
 window.addEventListener("deviceorientation", on_orientation_uab);
-window.addEventListener("deviceorientation", on_motion_uab);
+window.addEventListener("devicemotion", on_motion_uab);
 
 function on_orientation_uab(e)
 {
