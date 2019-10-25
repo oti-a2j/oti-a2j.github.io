798b4be41f124d9374967be2cf9eb62a93a646e7
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-10-25 13:33:54 +0300

    acceleration adding

--- HTML/GYRO/gyro.js
+++ HTML/GYRO/gyro.js
@@ -1,8 +1,18 @@
-window.addEventListener("deviceorientation", on_roate_uab);
+document.getElementById("id_logic_version").innerHTML = "Logic Version = 2019.10.25.0";
 
-function on_roate_uab(e)
+window.addEventListener("deviceorientation", on_orientation_uab);
+window.addEventListener("deviceorientation", on_motion_uab);
+
+function on_orientation_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML = e.alpha;
 	document.getElementById("id_beta").innerHTML = e.beta;
 	document.getElementById("id_gamma").innerHTML = e.gamma;
+}
+
+function on_motion_uab(e)
+{
+	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
 }
