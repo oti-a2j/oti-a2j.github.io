dc9fe46a689f23a2e9a68798e19a58cf41708b2c
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-10-25 14:09:50 +0300

    Add files via upload

--- HTML/GYRO/gyro.js
+++ HTML/GYRO/gyro.js
@@ -5,14 +5,14 @@ window.addEventListener("devicemotion", on_motion_uab);
 
 function on_orientation_uab(e)
 {
-	document.getElementById("id_alpha").innerHTML = e.alpha;
-	document.getElementById("id_beta").innerHTML = e.beta;
-	document.getElementById("id_gamma").innerHTML = e.gamma;
+	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha *100)/100;
+	document.getElementById("id_beta").innerHTML = Math.round(e.beta *100)/100;
+	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma *100)/100;
 }
 
 function on_motion_uab(e)
 {
-	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
-	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
-	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+	document.getElementById("id_acc_z").innerHTML = Math.round(e.accelerationIncludingGravity.z*100)/100;
+	document.getElementById("id_acc_x").innerHTML = Math.round(e.accelerationIncludingGravity.x*100)/100;
+	document.getElementById("id_acc_y").innerHTML = Math.round(e.accelerationIncludingGravity.y*100)/100;
 }
