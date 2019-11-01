0557ea07b1583ead99edb084ad0556684a602f89
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-11-01 14:45:58 +0200

    Add files via upload

--- HTML/WORKER/desenare.js
+++ HTML/WORKER/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "Logic version = 2019.11.1.3";
+document.getElementById("id_logic").innerHTML = "Logic version = 2019.11.1.4";
 
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
