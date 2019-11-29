e1e0aa101cdf0cca3931434ff3c9390ab75eeab4
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-11-29 13:22:32 +0200

    Add files via upload

--- HTML/TOUCH/touch.js
+++ HTML/TOUCH/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "2019.11.29.0";
+document.getElementById("id_logic_version").innerHTML = "2019.11.29.1";
 
 window.addEventListener("touchstart", touch_start_uab);
 
@@ -7,14 +7,24 @@ var context = canvas.getContext("2d");
 
 var client_rect = canvas.getBoundingClientRect();
 
+function get_random_color()
+{
+	var tmp = "0123456789ABCDEF";
+	var culoare = "#";
+	for (var i = 0; i < 6; i++) 
+	{
+		culoare += tmp[Math.floor(Math.random() * 16)];
+	}
+	return culoare;
 
-function touch_start_uab(e)
 {
 	var t = e.changedTouches;
 	for (var i = 0; i < t.length; i++)
 	{
 		context.beginPath();
 		context.arc(t[i].pageX - client_rect.left, t[i].pageY - client_rect.top, 10, 0, 2 * Math.PI);
+		context.fillStyle = get_random_color();
+		context.strokeStyle = context.fillStyle;
 		context.stroke();
 	}
 }
