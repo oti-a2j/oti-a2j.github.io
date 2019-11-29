d8f7c872b3967c3c487e266874cafc3088c24c57
oti-a2j <56252160+oti-a2j@users.noreply.github.com>
2019-11-29 14:10:11 +0200

    Add files via upload

--- HTML/TOUCH/touch.js
+++ HTML/TOUCH/touch.js
@@ -51,7 +51,7 @@ function touch_move_uab(e)
 		var touch_index = -1;
 		for (var j=0; j< last_touch.length; j++)
 		{
-			if (t[i].identifier == last_touch[j].identifier) 
+			if (t[i].identifier == last_touch[j].id) 
 			{
 				touch_index = j
 				break;
